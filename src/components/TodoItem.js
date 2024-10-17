287b59f6c68d731eb1425eb4308c2d5288507699
Sunil Park <sunilpark1129@gmail.com>
2024-10-17 11:34:07 -0400

    first commit

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,54 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import api from "../utils/api";
 
-const TodoItem = () => {
+const TodoItem = ({ item, setTodoList }) => {
+  const { _id, task, isComplete } = item;
+  const complete = isComplete ? "끝남" : "안끝남";
+
+  const updateTask = async () => {
+    try {
+      const response = await api.put(`/tasks/${_id}`, { isComplete });
+
+      if (response.status === 200) {
+        const res = await api.get("/tasks");
+        setTodoList(res.data.data);
+      } else {
+        throw new Error();
+      }
+    } catch (err) {
+      console.log("error", err);
+    }
+  };
+
+  const deleteTask = async () => {
+    try {
+      const response = await api.delete(`/tasks/${_id}`, { isComplete });
+
+      if (response.status === 200) {
+        const res = await api.get("/tasks");
+        setTodoList(res.data.data);
+      } else {
+        throw new Error();
+      }
+    } catch (err) {
+      console.log("error", err);
+    }
+  };
+
   return (
     <Row>
       <Col xs={12}>
         <div className={`todo-item`}>
-          <div className="todo-content">밥먹기</div>
+          <div className="todo-content">{task}</div>
 
           <div>
-            <button className="button-delete">삭제</button>
-            <button className="button-delete">끝남</button>
+            <button className="button-delete" onClick={deleteTask}>
+              삭제
+            </button>
+            <button className="button-delete" onClick={updateTask}>
+              {complete}
+            </button>
           </div>
         </div>
       </Col>
